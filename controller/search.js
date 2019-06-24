dac97ea0735a8753167271308d05bb0e0267b687
rq <1508477509@qq.com>
2019-06-24 20:01:22 +0800

    ''

--- controller/search.js
+++ controller/search.js
@@ -3,6 +3,7 @@ const connection = require('../database/db');
 
 async function getSearchResult(ctx) {
     let {keyword} = ctx.query;
+    let actionPart = await connection.query('select * from action where ');
 }
 
 module.exports = {
