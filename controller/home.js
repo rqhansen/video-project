84cf8aca32dca1f779acce580e7da4372fd49a0c
rqhansen <1508477509@qq.com>
2019-06-26 09:29:33 +0800

    ''

--- controller/home.js
+++ controller/home.js
@@ -1,7 +1,7 @@
 'use strict';
 const connection = require('../database/db');
-const {movieTypes}  =require('../config/index');
-const {formatDate} = require('./utils');
+const { movieTypes, limitSeconds } = require('../config/index');
+const { formatDate } = require('./utils');
 
 /**
  * 获取首页电影列表
@@ -10,9 +10,10 @@ const {formatDate} = require('./utils');
 async function getHome(ctx) {
     //数据库存在数据，则从数据库获取
     let movies = await connection.query('select * from films order by pubDate desc limit 24 offset 0');
-    movies.forEach(item =>{
-        item.filmType = Object.values(movieTypes.filter(type =>type[`${item.typeId}`])[0])[0];
+    movies.forEach(item => {
+        item.filmType = Object.values(movieTypes.filter(type => type[`${item.typeId}`])[0])[0];
         item.pubDate = formatDate(item.pubDate);
+        item.isNew = new Date() - new Date(item.pubDate) <= limitSeconds;
     })
     await ctx.render('index', { movies });
 }
