7f631808b13ba3461ccce645a552b7976d887c09
rq <1508477509@qq.com>
2019-06-30 17:41:33 +0800

    ''

--- config/index.js
+++ config/index.js
@@ -1,8 +1,12 @@
 
 module.exports.limitSeconds = 259200000;
 module.exports.connectConfig = { //数据库连接池的配置
-    host: '127.0.0.1',
-    user: 'root',
+    // host: '127.0.0.1',
+    // user: 'root',
+    // password: 'admin',
+    // database: 'movie'
+    host: '154.92.17.228',
+    user: 'movie',
     password: 'admin',
     database: 'movie'
 }
