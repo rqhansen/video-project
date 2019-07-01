e18da2575f1fa16a2ce77d0ca7397427a2861e2e
rqhansen <1508477509@qq.com>
2019-07-02 00:38:57 +0800

    ''

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('koa-router')();
 const { getHome } = require('../controller/home');
+const { getTodaySources } = require('../controller/todaySources');
+const { getWeekSources } = require('../controller/weekSources');
 const { getMovieDetail } = require('../controller/mDetail');
 const { getMore } = require('../controller/more');
 const { getTypeMovies } = require('../controller/typeMovie');
-const {getSearchResult} = require('../controller/search');
-const {getNotFind} = require('../controller/notFind');
+const { getSearchResult } = require('../controller/search');
+const { getNotFind } = require('../controller/notFind');
 const typeExp = '(action|comedy|romance|science|drama|suspense|horror|war|horror|disaster)/(index|page_\\d+)';
 const detailExp = '(action|comedy|romance|science|drama|suspense|horror|war|horror|disaster)/(\\d+)';
 
@@ -14,10 +16,21 @@ const detailExp = '(action|comedy|romance|science|drama|suspense|horror|war|horr
 router.get('/', getHome);
 
 /**
+ * 今日最新电影
+ */
+router.get('/html/today', getTodaySources);
+
+/**
+ * 近一周最新电影
+ */
+router.get('/html/week', getWeekSources);
+
+/**
  * 更多
  */
 
 router.get('/html/more', getMore);
+
 
 /**
  * 类别电影
@@ -33,12 +46,12 @@ router.get(`/html/${detailExp}`, getMovieDetail);
 /**
  * 搜索
  */
-router.get('/plus/search',getSearchResult);
+router.get('/plus/search', getSearchResult);
 
 /**
  * 404页面
  */
-router.get('*',getNotFind);
+router.get('*', getNotFind);
 
 /**
  * 指定类型的电影列表
