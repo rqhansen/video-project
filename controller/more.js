0b4862afe8858b1a2d5a83be5d63083b8e24b809
rq <1508477509@qq.com>
2019-07-02 13:39:37 +0800

    ''

--- controller/more.js
+++ controller/more.js
@@ -1,13 +1,12 @@
 'use strict';
 const connection = require('../database/db');
 const { movieTypes,limitSeconds } = require('../config/index');
-const { formatDate } = require('./utils');
 
 async function getMore(ctx) {
-    let movies = await connection.query('select * from films order by pubDate desc limit 102 offset 0');
+    let movies = await connection.query('select id,typeId,indexImgSrc,date_format(pubDate,"%Y-%m-%d"),fullName from films order by pubDate desc limit 102 offset 0');
     movies.forEach(item => {
         item.filmType = Object.values(movieTypes.filter(type => type[`${item.typeId}`])[0])[0];
-        item.pubDate = formatDate(item.pubDate);
+        item.pubDate = item['date_format(pubDate,"%Y-%m-%d")'];
         item.isNew = new Date() - new Date(item.pubDate) <= limitSeconds;
     })
     await ctx.render('more', { movies });
