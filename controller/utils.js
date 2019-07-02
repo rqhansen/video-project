43173777495bbf209b2f6a26887e6c9d29cf67ef
rqhansen <1508477509@qq.com>
2019-07-02 23:36:24 +0800

    ''

--- controller/utils.js
+++ controller/utils.js
@@ -1,9 +1,11 @@
+const { movieTypes, limitSeconds } = require('../config/index');
+
 
 /**
  * 日期转为2019-07-03格式
  * @param {String} dateStr 日期
  */
-module.exports.formatDate = function (date) {
+function formatDate(date) {
     let y = date.getFullYear();
     let m = date.getMonth() + 1;
     let dt = date.getDate();
@@ -11,6 +13,24 @@ module.exports.formatDate = function (date) {
     dt = dt >= 10 ? dt : `0${dt}`;
     return `${y}-${m}-${dt}`;
 }
+
+/**
+ * 处理获取首页电影列表后的数据
+ */
+function handleMovieList(movies) {
+    movies.forEach(item => {
+        item.filmType = Object.values(movieTypes.filter(type => type[`${item.typeId}`])[0])[0];
+        item.pubDate = formatDate(item.pubDate);
+        item.isNew = new Date() - new Date(item.pubDate) <= limitSeconds;
+    })
+    return movies;
+}
+
+module.exports = {
+    formatDate,
+    handleMovieList
+}
+
 
 
 
--- controller/weekSources.js
+++ controller/weekSources.js
@@ -1,22 +1,60 @@
 'use strict';
 const connection = require('../database/db');
 const { movieTypes } = require('../config/index');
-const { formatDate } = require('./utils');
+const { handleMovieList } = require('./utils');
 
 async function getWeekSources(ctx) {
-    let results = await connection.query(`select * from action where date_sub(curdate(),interval 7 day) <= date(pubDate)
-                                        union select * from comedy where date_sub(curdate(),interval 7 day) <= date(pubDate)
-                                        union select * from drama where date_sub(curdate(),interval 7 day) <= date(pubDate)
-                                        union select * from horror where date_sub(curdate(),interval 7 day) <= date(pubDate)
-                                        union select * from romance where date_sub(curdate(),interval 7 day) <= date(pubDate)
-                                        union select * from science where date_sub(curdate(),interval 7 day) <= date(pubDate)
-                                        union select * from suspense where date_sub(curdate(),interval 7 day) <= date(pubDate)
-                                        union select * from war where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
-    results.forEach(item => {
-        item.filmType = Object.values(movieTypes.filter(type => type[`${item.typeId}`])[0])[0];
-        item.pubDate = formatDate(item.pubDate);
-        item.isNew = true;
-    })
+    let results = [];
+    let action = await connection.query(`select * from action where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    let comedy = await connection.query(`select * from comedy where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    let romance = await connection.query(`select * from romance where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    let science = await connection.query(`select * from science where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    let drama = await connection.query(`select * from drama where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    let suspense = await connection.query(`select * from suspense where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    let war = await connection.query(`select * from war where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    let horror = await connection.query(`select * from horror where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    let disaster = await connection.query(`select * from disaster where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    if (action.length) {
+        results.push(handleMovieList(action));
+    }
+    if (comedy.length) {
+        results.push(handleMovieList(comedy));
+    }
+    if (romance.length) {
+        results.push(handleMovieList(romance));
+    }
+    if (science.length) {
+        results.push(handleMovieList(science));
+    }
+    if (drama.length) {
+        results.push(handleMovieList(drama));
+    }
+    if (suspense.length) {
+        results.push(handleMovieList(suspense));
+    }
+    if (war.length) {
+        results.push(handleMovieList(war));
+    }
+    if (horror.length) {
+        results.push(handleMovieList(horror));
+    }
+    if (disaster.length) {
+        results.push(handleMovieList(disaster));
+    }
+    // console.log(results);
+    // let results = await connection.query(`select * from action where date_sub(curdate(),interval 7 day) <= date(pubDate)
+    //                                     union select * from comedy where date_sub(curdate(),interval 7 day) <= date(pubDate)
+    //                                     union select * from drama where date_sub(curdate(),interval 7 day) <= date(pubDate)
+    //                                     union select * from horror where date_sub(curdate(),interval 7 day) <= date(pubDate)
+    //                                     union select * from romance where date_sub(curdate(),interval 7 day) <= date(pubDate)
+    //                                     union select * from science where date_sub(curdate(),interval 7 day) <= date(pubDate)
+    //                                     union select * from suspense where date_sub(curdate(),interval 7 day) <= date(pubDate)
+    //                                     union select * from war where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
+    // results.forEach(item => {
+    //     item.filmType = Object.values(movieTypes.filter(type => type[`${item.typeId}`])[0])[0];
+    //     item.pubDate = formatDate(item.pubDate);
+    //     item.isNew = true;
+    // })
     await (ctx.render('weekSources', { results }));
 }
 
