ec4b5e91a07675ac7df5151995322aae93d879ab
rqhansen <1508477509@qq.com>
2019-07-03 23:10:35 +0800

    ''

--- controller/more.js
+++ controller/more.js
@@ -1,14 +1,21 @@
 'use strict';
 const connection = require('../database/db');
-const { movieTypes,limitSeconds } = require('../config/index');
+const { movieTypes, limitSeconds } = require('../config/index');
 
 async function getMore(ctx) {
-    let movies = await connection.query('select id,typeId,indexImgSrc,date_format(pubDate,"%Y-%m-%d"),fullName from films order by pubDate desc limit 102 offset 0');
+    let movies = await connection.query(`select id,typeId,indexImgSrc,fullName, date_format(pubDate,"%Y-%m-%d"),pubDate from action where date_sub(curdate(),interval 30 day) <= date(pubDate)
+                                        union select id,typeId,indexImgSrc,fullName, date_format(pubDate,"%Y-%m-%d"),pubDate from comedy where date_sub(curdate(),interval 30 day) <= date(pubDate)
+                                        union select id,typeId,indexImgSrc,fullName, date_format(pubDate,"%Y-%m-%d"),pubDate from drama where date_sub(curdate(),interval 30 day) <= date(pubDate)
+                                        union select id,typeId,indexImgSrc,fullName, date_format(pubDate,"%Y-%m-%d"),pubDate from horror where date_sub(curdate(),interval 30 day) <= date(pubDate)
+                                        union select id,typeId,indexImgSrc,fullName, date_format(pubDate,"%Y-%m-%d"),pubDate from romance where date_sub(curdate(),interval 30 day) <= date(pubDate)
+                                        union select id,typeId,indexImgSrc,fullName, date_format(pubDate,"%Y-%m-%d"),pubDate from science where date_sub(curdate(),interval 30 day) <= date(pubDate)
+                                        union select id,typeId,indexImgSrc,fullName, date_format(pubDate,"%Y-%m-%d"),pubDate from suspense where date_sub(curdate(),interval 30 day) <= date(pubDate)
+                                        union select id,typeId,indexImgSrc,fullName, date_format(pubDate,"%Y-%m-%d"),pubDate from war where date_sub(curdate(),interval 30 day) <= date(pubDate) order by pubDate desc`);
     movies.forEach(item => {
         item.filmType = Object.values(movieTypes.filter(type => type[`${item.typeId}`])[0])[0];
         item.pubDate = item['date_format(pubDate,"%Y-%m-%d")'];
         item.isNew = new Date() - new Date(item.pubDate) <= limitSeconds;
-        delete item['date_format(pubDate,"%Y-%m-%d")']
+        delete item['date_format(pubDate,"%Y-%m-%d")'];
     })
     await ctx.render('more', { movies });
 }
