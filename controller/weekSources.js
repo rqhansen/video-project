d88e8050e93026e56a300e124237d50e7ca0b97a
rq <1508477509@qq.com>
2019-07-03 10:12:01 +0800

    ''

--- controller/weekSources.js
+++ controller/weekSources.js
@@ -1,6 +1,5 @@
 'use strict';
 const connection = require('../database/db');
-const { movieTypes } = require('../config/index');
 const { handleMovieList } = require('./utils');
 
 async function getWeekSources(ctx) {
@@ -41,20 +40,6 @@ async function getWeekSources(ctx) {
     if (disaster.length) {
         results.push(handleMovieList(disaster));
     }
-    console.log(results);
-    // let results = await connection.query(`select * from action where date_sub(curdate(),interval 7 day) <= date(pubDate)
-    //                                     union select * from comedy where date_sub(curdate(),interval 7 day) <= date(pubDate)
-    //                                     union select * from drama where date_sub(curdate(),interval 7 day) <= date(pubDate)
-    //                                     union select * from horror where date_sub(curdate(),interval 7 day) <= date(pubDate)
-    //                                     union select * from romance where date_sub(curdate(),interval 7 day) <= date(pubDate)
-    //                                     union select * from science where date_sub(curdate(),interval 7 day) <= date(pubDate)
-    //                                     union select * from suspense where date_sub(curdate(),interval 7 day) <= date(pubDate)
-    //                                     union select * from war where date_sub(curdate(),interval 7 day) <= date(pubDate)`);
-    // results.forEach(item => {
-    //     item.filmType = Object.values(movieTypes.filter(type => type[`${item.typeId}`])[0])[0];
-    //     item.pubDate = formatDate(item.pubDate);
-    //     item.isNew = true;
-    // })
     await (ctx.render('weekSources', { results }));
 }
 
