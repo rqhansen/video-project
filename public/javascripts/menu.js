8b03966b9a86d805311cb7c4a21bed4ec35e7b45
rqhansen <1508477509@qq.com>
2019-07-05 09:24:08 +0800

    ''

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,51 +1,52 @@
 
-(function(){
+(function () {
     let menuWp = selectEleById('menuWp');
-    menuWp.addEventListener('click',(e) =>{
+    menuWp.addEventListener('click', (e) => {
+        if (!hasClassName(e.target, 'work')) return;
         handleTypeMovie(e);
-    },false);
-/**
- * 处理不同的电影类型事件
- */
-function handleTypeMovie(e) {
-    let [val,url] = [e.target.innerHTML,'/html'];
-    switch (val) {
-        case '首页':
-            url='/';
-            break;
-        case '动作片':
-            url += '/action/index';
-            break;
-        case '喜剧片':
-            url += '/comedy/index';
-            break;
-        case '爱情片':
-            url += '/romance/index';
-            break;
-        case '科幻片':
-            url += '/science/index';
-            break;
-        case '剧情片':
-            url += '/drama/index';
-            break;
-        case '悬疑片':
-            url += '/suspense/index';
-            break;
-        case '战争片':
-            url += '/war/index';
-            break;
-        case '恐怖片':
-            url += '/horror/index';
-            break;
-        case '灾难片':
-            url += '/disaster/index';
-            break;
-        case '伦理片':
-            url += '/ethical/index';
-        default:
-            break;
+    }, false);
+    /**
+     * 处理不同的电影类型事件
+     */
+    function handleTypeMovie(e) {
+        let [val, url] = [e.target.innerHTML, '/html'];
+        switch (val) {
+            case '首页':
+                url = '/';
+                break;
+            case '动作片':
+                url += '/action/index';
+                break;
+            case '喜剧片':
+                url += '/comedy/index';
+                break;
+            case '爱情片':
+                url += '/romance/index';
+                break;
+            case '科幻片':
+                url += '/science/index';
+                break;
+            case '剧情片':
+                url += '/drama/index';
+                break;
+            case '悬疑片':
+                url += '/suspense/index';
+                break;
+            case '战争片':
+                url += '/war/index';
+                break;
+            case '恐怖片':
+                url += '/horror/index';
+                break;
+            case '灾难片':
+                url += '/disaster/index';
+                break;
+            case '伦理片':
+                url += '/ethical/index';
+            default:
+                break;
+        }
+        window.open(url, '_self');
     }
-    window.open(url,'_self');
-}
 })();
 
