6c45107c6e9ab77d6d52e02ae22dcfd3f041c464
rqhansen <1508477509@qq.com>
2019-07-14 09:28:12 +0800

    ''

--- controller/downLoadThurder.js
+++ controller/downLoadThurder.js
@@ -1,14 +1,15 @@
 'use strict';
-const send = require('koa-send');
+// const send = require('koa-send');
 const { thurderUrl } = require('../config/index');
 
 
 async function downLoadThurder(ctx) {
-    const fileName = 'xunlei5.zip';
+    // const fileName = 'xunlei5.zip';
     // const fileName = 'Pc版+迅雷+v5.+绿色共存版【可下载版权限制的资源】.zip';
-    ctx.attachment(fileName);
-    await send(ctx, fileName, { root: thurderUrl });
+    // ctx.attachment(fileName);
+    // await send(ctx, fileName, { root: thurderUrl });
     // await ctx
+    await ctx.render('downLoadThurder');
 }
 
 module.exports = {
