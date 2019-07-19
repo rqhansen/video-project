c28c91e3a361a8f7f0ef7caa50c2d373774f5123
rqhansen <1508477509@qq.com>
2019-07-20 06:54:09 +0800

    ''

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -210,5 +210,27 @@ function throttle(fn, delay = 200) {
     }
 }
 
+/**
+ * 检测客户端是PC还是手机其它型号
+ */
+function userAgent() {
+    var ua = navigator.userAgent,
+        isWindowsPhone = /(?:Windows Phone)/.test(ua),
+        isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+        isAndroid = /(?:Android)/.test(ua),
+        isFireFox = /(?:Firefox)/.test(ua),
+        isChrome = /(?:Chrome|CriOS)/.test(ua),
+        isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+        isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+        isPc = !isPhone && !isAndroid && !isSymbian;
+    return {
+        isTablet: isTablet,
+        isPhone: isPhone,
+        isAndroid: isAndroid,
+        isPc: isPc
+    };
+}
+
+
 
 
