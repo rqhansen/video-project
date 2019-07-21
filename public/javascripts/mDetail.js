34d0d9a71e60d0713d55879d72868f975768759e
rqhansen <1508477509@qq.com>
2019-07-22 07:32:57 +0800

    ''

--- public/javascripts/mDetail.js
+++ public/javascripts/mDetail.js
@@ -1,29 +1,29 @@
 //控制内容显示隐藏
-(function(){
+(function () {
     let detail = selectEleById('detailContent');
     let ps = detail.getElementsByTagName('p');
-    Array.from(ps).forEach(el =>{
+    Array.from(ps).forEach(el => {
         let span = el.children[0];
-        if(!span.innerText) {
-            addClassName(span.parentNode,'hide');
+        if (!span.innerText) {
+            addClassName(span.parentNode, 'hide');
         }
     })
     //复制影片链接(pc和移动端)
-    let {width} = getClientSize();
-    if(width >767) {
+    let { width } = getClientSize();
+    if (width > 767) {
         copyMDetail('copyMDetail');
     } else {
         copyMDetail('mbCopyMDetail');
     }
-    function copyMDetail(eleId){
+    function copyMDetail(eleId) {
         let cBtn = selectEleById(eleId);
-        cBtn.addEventListener('click',() =>{
-            let url =  window.location.href;
+        cBtn.addEventListener('click', () => {
+            let url = window.location.href;
             let flag = copyText(url);
-            if(flag) {
-                alert('网址已复制，粘贴给好友请按 Crtl+V');
+            if (flag) {
+                alert('网址已复制，分享给好友了!');
             }
-        },false);
+        }, false);
     }
 })();
 
