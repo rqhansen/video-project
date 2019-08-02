58b8f65709176ca8aaee8ed61819a7a847ecbbe8
rqhansen <1508477509@qq.com>
2019-08-02 23:23:54 +0800

    ''

--- public/javascripts/searchResult.js
+++ public/javascripts/searchResult.js
@@ -2,18 +2,18 @@
 (function () {
     let isPc = isPcAgent();
     let mainEle = selectEleById('main');
-    if(isPc) {
-        addEvent(document,'scroll',throttle(issShowPcGoTop));
+    if (isPc) {
+        addEvent(document, 'scroll', throttle(issShowPcGoTop));
         function issShowPcGoTop() {
-        let scrollTop = getScrollTop();
-        if (scrollTop >= 300) {
-            addClassName(goTop, 'show');
-        } else {
-            removeClassName(goTop, 'show');
-        }
+            let scrollTop = getScrollTop();
+            if (scrollTop >= 300) {
+                addClassName(goTop, 'show');
+            } else {
+                removeClassName(goTop, 'show');
+            }
         }
     } else {
-        addEvent(mainEle,'scroll',throttle(isShowGoTop));
+        addEvent(mainEle, 'scroll', throttle(isShowGoTop));
         function isShowGoTop() {
             let scrollTop = mainEle.scrollTop;
             if (scrollTop >= 300) {
@@ -21,11 +21,11 @@
             } else {
                 removeClassName(goTop, 'show');
             }
-        } 
+        }
     }
     //总页数
     let total = Math.ceil(+selectEleById('totalPage').innerHTML / 10);
-    if(!total) return;
+    if (!total) return;
     //分页容器
     let pageWp = selectEleById('pageIndexWp');
     //首页
@@ -40,7 +40,6 @@
     let last = selectElesByClassName('end', pageWp)[0];
     //获取当前页数
     let [keyword, currPage] = getCurrPage();
-    let 
     //初始化
     if (currPage === 1) { //首页
         initOtherPage();
